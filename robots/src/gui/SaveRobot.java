a71860e8be07eae914765c74faad019e0df85b95
Pavel <37647709+pk8642@users.noreply.github.com>
2018-05-28 23:55:18 +0500

    мелкие поправки

--- robots/src/gui/SaveRobot.java
+++ robots/src/gui/SaveRobot.java
@@ -25,4 +25,11 @@ public class SaveRobot implements Serializable {
         this.orientation = gameWindow.getDirection();
     }
 
+    public SaveRobot(RobotMove robot){
+        this.x = robot.m_robotPositionX;
+        this.y=robot.m_robotPositionY;
+        this.aim=new Point(robot.m_targetPositionX,robot.m_targetPositionY);
+        this.orientation=robot.m_robotDirection;
+    }
+
 }
